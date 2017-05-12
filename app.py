8941d8426964121656a63564087d24b6fda1308b
ThomE-immalle <thom.ernst@immalle.be>
2017-05-12 21:56:08 +0200

    You now go to the quiz.html page after a succesful login

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, json, request, redirect, url_for
 from flaskext.mysql import MySQL
-#from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'imma'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'imma'
-app.config['MYSQL_DATABASE_DB'] = 'BucketList'
+app.config['MYSQL_DATABASE_DB'] = 'GIP-Schema'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
@@ -23,18 +22,42 @@ def showSignUp():
 
 @app.route('/signUp', methods=['POST'])
 def signUp():
-    _name = request.form['inputName']
-    _email = request.form['inputEmail']
-    _password = request.form['inputPassword']
+    try:
+        p_name = request.form['inputName']
+        p_lastname = request.form['inputLastname']
 
-    con = mysql.connect()
-    cur = con.cursor()
-    #_hashed_password = generate_password_hash(_password)
-    cur.callproc('sp_createUser', (_name, _email, _password))
-    data = cur.fetchall()
+        con = mysql.connect()
+        cur = con.cursor()
+        cur.callproc('sp_createUser', (p_name, p_lastname))
+        data = cur.fetchall()
 
-    if len(data) is 0:
-        con.commit()
-        return json.dumps({'message': 'User created successfully !'})
-    else:
-        return json.dumps({'error': str(data[0])})
+        if len(data) is 0:
+            con.commit()
+            return json.dumps({'message': 'User created successfully !'})
+        else:
+            return json.dumps({'error': str(data[0])})
+
+    except Exception as e:
+        return json.dumps({'error': str(e)})
+
+    finally:
+        if con and cur:
+            cur.close()
+            con.close()
+
+
+@app.route('/quizApp')
+def quizApp():
+    return render_template('quiz.html')
+
+
+@app.route('/checkAnswer', methods=['GET'])
+def checkAnswer():
+    try:
+        answer = request.data
+
+        if answer == 1:
+            return 'ham'
+
+    except Exception as e:
+        return 'foo bar'
